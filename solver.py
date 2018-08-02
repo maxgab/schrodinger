9152cd2d0416bec3d79f0bd72312e2dc7052db95
Max Gaber <maxgaber@uni-bremen.de>
2018-08-02 10:08:17 +0200

    initial commit

---
+++ solver.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+"""solves the schrodinger equation for given parameters in /input/schrodinger.inp"""
---
+++ visualizer.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+"""visualizes the results from solver.py given in the output folder"""
